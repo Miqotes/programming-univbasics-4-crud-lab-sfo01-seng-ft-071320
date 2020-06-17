af20f9455d33cd9e854b81784337a92ffaf0af9f
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-17 23:59:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  onehundredarrays = ["arrays", "tell", "we", "sleep"]
+  onehundredarrays.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
