5334c036a5dc88f830deb717a02df536b87d3b9d
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-17 23:51:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  mycats = ["chonk", "little cat"]
+  mycats << "mr.nug"
+  mycats
 end
 
 def add_element_to_start_of_array(array, element)
