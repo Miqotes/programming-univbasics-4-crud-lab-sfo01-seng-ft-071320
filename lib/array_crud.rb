07a269fa58790b69d4713a6a697e5d1efa37dd67
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-18 00:04:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  helloarray = ["This time", "the array", "will end with", "arrays!"]
+  helloarray[-1]
 end
 
 def update_element_from_index(array, index_number, element)
