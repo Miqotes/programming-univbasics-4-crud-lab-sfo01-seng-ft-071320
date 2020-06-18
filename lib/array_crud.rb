f526820bb3bb942f18cbbe49c61166d569b016c5
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-18 00:01:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  thistime = ["I", "am", "will be cool with arrays."]
+  thistime[1]
 end
 
 def retrieve_first_element_from_array(array)
